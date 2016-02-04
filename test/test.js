69490811f91b963b75809359fdb4c44554dd93b9
JT Yim <JT@JTYim.com>
2016-02-04 15:11:52 -0500

    Initial routing + client requests

--- test/test.js
+++ test/test.js
@@ -71,7 +71,7 @@ describe("server", function() {
   });
 });
 
-describe("archive helpers", function(){
+xdescribe("archive helpers", function(){
   describe("#readListOfUrls", function () {
     it("should read urls from sites.txt", function (done){
       var urlArray = ["example1.com", "example2.com"];
@@ -84,7 +84,7 @@ describe("archive helpers", function(){
     });
   });
 
-  describe("#isUrlInList", function () {
+  xdescribe("#isUrlInList", function () {
     it("should check if a url is in the list", function (done) {
       var urlArray = ["example1.com", "example2.com"];
       fs.writeFileSync(archive.paths.list, urlArray.join("\n"));
@@ -104,7 +104,7 @@ describe("archive helpers", function(){
     });
   });
 
-  describe("#addUrlToList", function () {
+  xdescribe("#addUrlToList", function () {
     it("should add a url to the list", function (done) {
       var urlArray = ["example1.com", "example2.com\n"];
       fs.writeFileSync(archive.paths.list, urlArray.join("\n"));
@@ -118,7 +118,7 @@ describe("archive helpers", function(){
     });
   });
 
-  describe("#isUrlArchived", function () {
+  xdescribe("#isUrlArchived", function () {
     it("should check if a url is archived", function (done) {
       fs.writeFileSync(archive.paths.archivedSites + "/www.example.com", "blah blah");
 
@@ -137,7 +137,7 @@ describe("archive helpers", function(){
     });
   });
 
-  describe("#downloadUrls", function () {
+  xdescribe("#downloadUrls", function () {
     it("should download all pending urls in the list", function (done) {
       var urlArray = ["www.example.com", "www.google.com"];
       archive.downloadUrls(urlArray);
---
+++ web/sandbox.js
@@ -0,0 +1,28 @@
+
+
+var http = require("http")
+
+// new
+var handler = function(req,resp){
+    // new
+}
+
+var server = htpttp.create(handler)
+  server.lister
+
+
+  handler = fcuntion(res, res){
+    if( req.url==='/'){
+
+    }else if( req.url==='/save' ){
+        if( ==="post")
+          var saved = "";
+          req.on("data", function(chunk)){
+          saved += chunk
+        }
+      }
+
+    }
+
+
+  }
